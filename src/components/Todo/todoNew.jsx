fa286e417e464ed1d581bb40c44cce9fcdc16a51
DANG DUC <dangcongduc38@gmail.com>
2024-10-22 23:02:09 +0700

    video 36 DOM EVENTS --get data input

--- src/components/Todo/todoNew.jsx
+++ src/components/Todo/todoNew.jsx
@@ -3,10 +3,24 @@ const TodoNew = (props) => {
     console.log(">>>> check props add todo : ", props)
     const { addNewTodo } = props
     // addNewTodo("quet nha")
+    const handleAddTodo = () => {
+        addNewTodo("Clean House")
+    }
+
+    const handleInput = (data) => {
+        console.log(" >>> check input : ", data)
+    }
     return (
         <div className="todo-new">
-            <input className="input-todo" type="text" />
-            <button className="btn-add" onClick={addNewTodo}>ADD</button>
+            <input
+                className="input-todo"
+                type="text"
+                onChange={(event) => handleInput(event.target.value)} />
+            <button
+                className="btn-add"
+                style={{ cursor: "pointer" }}
+                onClick={handleAddTodo}
+            >ADD</button>
         </div>
     )
 }
