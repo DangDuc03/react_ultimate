1212a3e8c4bea5cce0a15505cac7eccb0df95313
DANG DUC <dangcongduc38@gmail.com>
2024-11-03 15:19:41 +0700

    used UnControlled Componentfor register by Antd

--- src/pages/screens/register.jsx
+++ src/pages/screens/register.jsx
@@ -1,6 +1,81 @@
+
+import React from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+
+
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+
+    const onFinish = (values) => {
+        console.log(values);
+    };
+
     return (
-        <div>Register Page</div>
+        <>
+            <Form
+                form={form}
+                layout='vertical' // chieu doc
+                onFinish={onFinish}
+            // onFinishFailed={onFinishFailed}
+            >
+
+                <div style={{ marginTop: "20px", padding: "0 20px" }}>
+                    <h1 style={{ color: "blue" }}>Register</h1>
+                    <Form.Item
+                        label="Full Name"
+                        name="fullName"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Full Name!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Email"
+                        name="email"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Email!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <Form.Item
+                        label="Password"
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Password!',
+                            },
+                        ]}
+                    >
+                        <Input.Password />
+                    </Form.Item>
+                    <Form.Item
+                        label="Phone"
+                        name="phone"
+                        rules={[
+                            {
+                                required: true,
+                                message: 'Please input your Phone!',
+                            },
+                        ]}
+                    >
+                        <Input />
+                    </Form.Item>
+                    <div>
+                        <Button onClick={() => form.submit()} type='primary'>Register</Button>
+                    </div>
+                </div>
+            </Form>
+        </>
+
     )
 }
 
